dbfbae4aa4e4bed0b214e7cfbf605a3433474079
Manuel Camacho <maedca@gmail.com>
2015-12-18 21:22:06 -0430

    add the class active to the menu elements

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,14 +1,14 @@
 <ul class="nav navbar-nav">
-    <li role="presentation">
-        <a href="http://teachme.app">Recientes</a>
+    <li role="presentation" @if(Route::is('tickets.latest')) class="active" @endif>
+        <a href={{route('tickets.latest')}}>Recientes</a>
     </li>
-    <li role="presentation">
-        <a href="http://teachme.app/populares">Populares</a>
+    <li role="presentation" @if(Route::is('tickets.popular')) class="active" @endif>
+        <a href={{route('tickets.popular')}}>Populares</a>
     </li>
-    <li role="presentation">
-        <a href="http://teachme.app/pendientes">Abiertas</a>
+    <li role="presentation" @if(Route::is('tickets.open')) class="active" @endif>
+        <a href={{route('tickets.open')}}>Abiertas</a>
     </li>
-    <li role="presentation">
-        <a href="http://teachme.app/tutoriales">Finalizadas</a>
+    <li role="presentation" @if(Route::is('tickets.closed')) class="active" @endif>
+        <a href={{route('tickets.closed')}}>Finalizadas</a>
     </li>
 </ul>
